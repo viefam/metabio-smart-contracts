e19891ad3af884777c6c50c274a93c81aa81fb0d
Viet <vietdht@yahoo.com.vn>
2022-05-01 22:57:44 +0700

    add metabio ERC20 token

--- contracts/MetaBioToken.sol
+++ contracts/MetaBioToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MetaBioToken is ERC20 {
     constructor() ERC20("MetaBioToken", "MBT") {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 100000 * 10 ** 18);
     }
 }

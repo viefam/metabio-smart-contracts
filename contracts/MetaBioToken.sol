a4bfff8b973313b789b85dd869a9ecf7e20ea42b
Viet <vietdht@yahoo.com.vn>
2022-05-01 22:49:45 +0700

    add metabio ERC20 token

---
+++ contracts/MetaBioToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MetaBioToken is ERC20 {
+    constructor() ERC20("MetaBioToken", "MBT") {
+        _mint(msg.sender, 100000);
+    }
+}
